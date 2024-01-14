5a1c289b489d129df938bb1e525bfded4f0eda4a
Eishalex2 <t.whitney.a@gmail.com>
2024-01-14 17:04:00 -0500

    Remove an un-needed useState import

--- src/pages/leaderboard.jsx
+++ src/pages/leaderboard.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import GameContext from '../context';
 import styles from '../styles/gameCard.module.css';
